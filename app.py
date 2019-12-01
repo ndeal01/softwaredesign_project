9513e59d69e0fda8426c3216c79805c727514321
trobertson15 <trobgb1892@gmail.com>
2019-12-01 11:23:32 -0600

    Added files for circulation table

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from wtforms import StringField, IntegerField, DateField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
-from datetime import datetime
+import datetime
+from datetime import date, timedelta
 import pymysql
 import secrets
 
@@ -34,17 +35,16 @@ class g2_materialtable(db.Model):
 
 class MaterialForm(FlaskForm):
     MaterialID = IntegerField('Material ID: ')
-    Title = StringField('Title:', validators=[DataRequired()])
+    Title = StringField('Title: ', validators=[DataRequired()])
     DateAdded = DateField('Date Added: ')
     LastModified = DateField('Date Last Modified: ')
     Genre = StringField('Genre: ', validators=[DataRequired()])
-    Author = StringField('Author:', validators=[DataRequired()])
-    ISBN = IntegerField('ISBN:', validators=[DataRequired()])
+    Author = StringField('Author: ', validators=[DataRequired()])
+    ISBN = IntegerField('ISBN: ', validators=[DataRequired()])
 
 @app.route('/')
 def index():
-    all_materials = g2_materialtable.query.all()
-    return render_template('index.html', materials=all_materials, pageTitle='SLPL Materials')
+    return render_template('index.html', pageTitle='South Liberty Public Library')
 
 @app.route('/materials')
 def materials():
@@ -58,20 +58,19 @@ def search_materials():
         search_value = form['search_string']
         search = "%{0}%".format(search_value)
         results = g2_materialtable.query.filter( or_(g2_materialtable.Title.like(search), g2_materialtable.Genre.like(search), g2_materialtable.Author.like(search), g2_materialtable.ISBN.like(search))).all()
-        return render_template('index.html', materials=results, pageTitle="Materials", legend ="Update A Material")
+        return render_template('materials.html', materials=results, pageTitle="Materials", legend ="Update A Material")
     else:
         return redirect('/')
 
 @app.route('/add_material', methods=['GET', 'POST'])
 def add_material():
     form = MaterialForm()
-    print('Before validate')
     if form.validate_on_submit():
-        print('inside validate')
-        material = g2_materialtable(Title=form.Title.data, DateAdded=datetime.datetime.now(),LastModified=datetime.datetime.now(), Genre=form.Genre.data, Author=form.Author.data, ISBN=form.ISBN.data)
+        material = g2_materialtable(Title=form.Title.data, DateAdded=form.DateAdded.data,LastModified=datetime.datetime.now(), Genre=form.Genre.data, Author=form.Author.data, ISBN=form.ISBN.data)
         db.session.add(material)
         db.session.commit()
-        return redirect('/')
+        flash('Material was successfully added!')
+        return redirect("/materials")
     return render_template('add_material.html', form=form, pageTitle='Add A New Material', legend="Add A New Material")
 
 
@@ -86,9 +85,8 @@ def update_material(MaterialID):
     form = MaterialForm()
 
     if form.validate_on_submit():
-        material.MaterialID = form.MaterialID.data
         material.Title = form.Title.data
-        material.DateAdded = datetime.datetime.now()
+        material.DateAdded = form.DateAdded.data
         material.LastModified = datetime.datetime.now()
         material.Genre = form.Genre.data
         material.Author = form.Author.data
@@ -113,7 +111,7 @@ def delete_material(MaterialID):
         db.session.delete(material)
         db.session.commit()
         flash('Material was successfully deleted!')
-        return redirect("/")
+        return redirect("/materials")
     else: #if it's a GET request, send them to the home page
         return redirect("/")
 
@@ -122,13 +120,13 @@ class g2_peopletable(db.Model):
     PeopleID = db.Column(db.Integer, primary_key=True)
     FirstName = db.Column(db.String(255))
     LastName = db.Column(db.String(255))
-    Birthdate = db.Column(db.DateTime)
+    Birthdate = db.Column(db.String(255))
     Address = db.Column(db.String(255))
     City = db.Column(db.String(255))
     State = db.Column(db.String(255))
-    Zip = db.Column(db.Integer())
-    PhoneNumber1 = db.Column(db.Integer())
-    PhoneNumber2 = db.Column(db.Integer())
+    Zip = db.Column(db.Integer)
+    PhoneNumber1 = db.Column(db.Integer)
+    PhoneNumber2 = db.Column(db.Integer)
     Email = db.Column(db.String(255))
 
     def __repr__(self):
@@ -136,15 +134,15 @@ class g2_peopletable(db.Model):
 
 class PeopleForm(FlaskForm):
     PeopleID = IntegerField('People ID: ')
-    FirstName = StringField('First Name:', validators=[DataRequired()])
-    LastName = StringField('Last Name:', validators=[DataRequired()])
-    Birthdate = DateField('Birthdate: ')
+    FirstName = StringField('First Name: ', validators=[DataRequired()])
+    LastName = StringField('Last Name: ', validators=[DataRequired()])
+    Birthdate = StringField('Birthdate: ', validators=[DataRequired()])
     Address = StringField('Address: ', validators=[DataRequired()])
-    City = StringField('City:', validators=[DataRequired()])
-    State = StringField('State:', validators=[DataRequired()])
-    Zip = IntegerField('Zip:', validators=[DataRequired()])
-    PhoneNumber1 = IntegerField('Phone Number 1:', validators=[DataRequired()])
-    PhoneNumber2 = IntegerField('Phone Number 2:')
+    City = StringField('City: ', validators=[DataRequired()])
+    State = StringField('State: ', validators=[DataRequired()])
+    Zip = IntegerField('Zip: ', validators=[DataRequired()])
+    PhoneNumber1 = IntegerField('Phone Number 1: ', validators=[DataRequired()])
+    PhoneNumber2 = IntegerField('Phone Number 2: ')
     Email = StringField('Email: ', validators=[DataRequired()])
 
 @app.route('/people')
@@ -167,58 +165,56 @@ def search_people():
 @app.route('/add_people', methods=['GET', 'POST'])
 def add_people():
     form = PeopleForm()
-    print('Before validate')
     if form.validate_on_submit():
-        print('inside validate')
-        people = g2_peopletable(FirstName=form.FirstName.data, LastName=form.LastName.data, Birthdate=datetime.Birthdate.now(), Address=form.Address.data, City=form.City.data, State=form.State.data, Zip=form.Zip.data, PhoneNumber1=form.PhoneNumber1.data, PhoneNumber2=form.PhoneNumber2.data, Email=form.Email.data)
+        people = g2_peopletable(FirstName=form.FirstName.data, LastName=form.LastName.data, Birthdate=form.Birthdate.data, Address=form.Address.data, City=form.City.data, State=form.State.data, Zip=form.Zip.data, PhoneNumber1=form.PhoneNumber1.data, PhoneNumber2=form.PhoneNumber2.data, Email=form.Email.data)
         db.session.add(people)
         db.session.commit()
-        return redirect('/')
-    return render_template('add_people.html', form=form, pageTitle='Add A New Patron', legend="Add A New Patron")
+        flash('Patron was successfully added!')
+        return redirect('/people')
+    return render_template('add_people.html', form=form, pageTitle='Add New Patron', legend="Add New Patron")
 
 
-@app.route('/people/<int:PeopleID>', methods=['GET','POST'])
+@app.route('/patron/<int:PeopleID>', methods=['GET','POST'])
 def patron(PeopleID):
-    people = g2_peopletable.query.get_or_404(PeopleID)
-    return render_template('patron.html', form=people, pageTitle='Patron Details')
+    patron = g2_peopletable.query.get_or_404(PeopleID)
+    return render_template('patron.html', form=patron, pageTitle='Patron Details')
 
 @app.route('/patron/<int:PeopleID>/update', methods=['GET','POST'])
-def update_people(PeopleID):
-    people = g2_peopletable.query.get_or_404(PeopleID)
+def update_patron(PeopleID):
+    patron = g2_peopletable.query.get_or_404(PeopleID)
     form = PeopleForm()
 
     if form.validate_on_submit():
-        people.PeopleID = form.PeopleID.data
-        people.FirstName = form.FirstName.data
-        people.LastName = form.LastName.data
-        people.Birthdate = datetime.Birthdate.now()
-        people.Address = form.Address.data
-        people.City = form.City.data
-        people.State = form.State.data
-        people.Zip = form.Zip.data
-        people.PhoneNumber1 = form.PhoneNumber1.data
-        people.PhoneNumber2 = form.PhoneNumber2.data
-        people.Email = form.Email.data
+        patron.FirstName = form.Firstname.data
+        patron.LastName = form.LastName.data
+        patron.Birthdate = form.Birthdate.data
+        patron.Address = form.Address.data
+        patron.City = form.City.data
+        patron.State = form.State.data
+        patron.Zip = form.Zip.data
+        patron.PhoneNumber1 = form.PhoneNumber1.data
+        patron.PhoneNumber2 = form.PhoneNumber2.data
+        patron.Email = form.Email.data
         db.session.commit()
 
         flash('This patron has been updated!')
-        return redirect(url_for('patron', PeopleID=people.PeopleID))
+        return redirect(url_for('patron', PeopleID=patron.PeopleID))
 
-    form.PeopleID.data = people.PeopleID
-    form.FirstName.data = people.FirstName
-    form.LastName.data = people.LastName
-    form.Birthdate.data = people.Birthdate
-    form.Address.data = people.Address
-    form.City.data = people.City
-    form.State.data = people.State
-    form.Zip.data = people.Zip
-    form.PhoneNumber1.data = people.PhoneNumber1
-    form.PhoneNumber2.data = people.PhoneNumber2
-    form.Email.data = people.Email
-    return render_template('update_people.html', form=form, pageTitle='Update People',legend="Update People")
+    form.PeopleID.data = patron.PeopleID
+    form.FirstName.data = patron.FirstName
+    form.LastName.data = patron.LastName
+    form.Birthdate.data = patron.Birthdate
+    form.Address.data = patron.Address
+    form.City.data = patron.City
+    form.State.data = patron.State
+    form.Zip.data = patron.Zip
+    form.PhoneNumber1.data = patron.PhoneNumber1
+    form.PhoneNumber2.data = patron.PhoneNumber2
+    form.Email.data = patron.Email
+    return render_template('update_patron.html', form=form, pageTitle='Update Patron',legend="Update People")
 
-@app.route('/people/<int:PeopleID>/delete', methods=['POST'])
-def delete_people(PeopleID):
+@app.route('/patron/<int:PeopleID>/delete', methods=['POST'])
+def delete_patron(PeopleID):
     if request.method == 'POST': #if it's a POST request, delete the patron from the database
         people = g2_peopletable.query.get_or_404(PeopleID)
         db.session.delete(people)
@@ -231,10 +227,11 @@ def delete_people(PeopleID):
 class circulationtable(db.Model):
     #__tablename__ = 'results'
     CheckoutID = db.Column(db.Integer, primary_key=True)
-    MaterialID = db.Column(db.Integer)
-    PeopleID = db.Column(db.Integer)
+    PeopleID = db.Column(db.Integer, db.ForeignKey('PeopleID'), nullable=False)
+    MaterialID = db.Column(db.Integer, db.ForeignKey('MaterialID'), nullable=False)
     Checkoutdate = db.Column(db.DateTime)
     Datedue = db.Column(db.DateTime)
+    
 def __repr__(self):
     return "id: {0} | People ID: {1} | Material ID: {2} | Checkout Date: {3} | Date Due: {4}".format(self.CheckoutID, self.PeopleID, self.MaterialID, self.Checkoutdate, self.Datedue)
 
@@ -250,10 +247,54 @@ def checkout():
     all_checkout = circulationtable.query.all()
     return render_template('checkout.html', checkout=all_checkout, pageTitle='Circulation List')
 
-@app.route('/checkout/<int:CheckoutID>', methods=['GET','POST'])
+@app.route('/add_checkout', methods=['GET', 'POST'])
+def add_checkout():
+    form = CheckoutForm()
+    if form.validate_on_submit():
+        checkout = circulationtable(PeopleID=form.PeopleID.data, Checkoutdate=datetime.datetime.now(), Datedue=datetime.datetime.timedelta(days=14))
+        db.session.add(checkout)
+        db.session.commit()
+        flash('Material was successfully added!')
+        return redirect("/materials")
+    return render_template('add_checkout.html', form=form, pageTitle='Add A New Material', legend="Add A New Material")
+
+@app.route('/circulation/<int:CheckoutID>', methods=['GET','POST'])
 def circulation(CheckoutID):
     checkout = circulationtable.query.get_or_404(CheckoutID)
-    return render_template('checkout.html', form=checkout, pageTitle='Circulation Details')
+    return render_template('circulation.html', form=checkout, pageTitle='Circulation Details')
+
+@app.route('/circulation/<int:CheckoutID>/update', methods=['GET','POST'])
+def update_checkout(CheckoutID):
+    circulation = g2_materialtable.query.get_or_404(CheckoutID)
+    form = CheckoutForm()
+
+    if form.validate_on_submit():
+        circulation.PeopleID = form.PeopleID.data
+        circulation.MaterialID = form.MaterialID.data
+        circulation.Checkoutdate = datetime.datetime.now()
+        circulation.Datedue = datetime.datetime.timedelta(days=14)
+        db.session.commit()
+        flash('This Checkout has been updated!')
+        return redirect(url_for('circulation', CheckoutID=circulation.CheckoutlID))
+
+    form.CheckoutID.data = circulation.CheckoutID
+    form.PeopleID.data = circulation.PeopleID
+    form.MaterialID.data = circulation.MaterialID
+    form.Checkoutdate.data = circulation.Checkoutdate
+    form.Datedue.data = circulation.Datedue
+    return render_template('update_checkout.html', form=form, pageTitle='Update Checkout',legend="Update Checkout")
+
+@app.route('/circulation/<int:CheckoutID>/delete', methods=['POST'])
+def delete_checkout(CheckoutID):
+    if request.method == 'POST': #if it's a POST request, delete the material from the database
+        checkout = circulationtable.query.get_or_404(MaterialID)
+        db.session.delete(checkout)
+        db.session.commit()
+        flash('Checkout was successfully deleted!')
+        return redirect("/checkout")
+    else: #if it's a GET request, send them to the home page
+        return redirect("/")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
